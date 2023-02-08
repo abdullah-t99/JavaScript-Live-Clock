6a90896afd89e47fc1266625a5a0d52b38be889f
abdullah-t99 <60823411+abdullah-t99@users.noreply.github.com>
2023-02-08 20:25:37 +0000

    Live time as a GUI

--- App.js
+++ App.js
@@ -7,17 +7,19 @@ function theTime() {
 
   if (hours > 12) {
     status = "PM";
+    hours = hours - 12;
   }
+
+  var time = hours + ":" + minutes + ":" + seconds + ":" + status;
 
   hours = hours < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  console.log(hours + ":" + minutes + ":" + seconds + ":" + status);
+  document.getElementById("clockGUI").innerText = time;
+  document.getElementById("clockGUI").textContent = time;
 
   setTimeout(theTime, 1000);
 }
-
-// export default theTime();
 
 theTime();
